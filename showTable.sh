53747da0a6beecd32e2a05804022a7ba846e46c7
Eman <emanmagdy961@gmail.com>
2020-02-09 01:39:24 +0200

    add show table function without fix a bug in it

--- showTable.sh
+++ showTable.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 function showTable {
-    echo "Show table content here"
+    printf "${YELLOW}${bold}Show Table Content Here${NC}\n"
+    cat $tableName 2> /dev/null
+    if [[ $? != 0 ]]
+    then
+    printf "${RED}${bold}Error Displaying Table${NC}"
+    fi
+	
 }
