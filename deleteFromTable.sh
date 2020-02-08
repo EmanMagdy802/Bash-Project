848110b5adb4ca0522da28ebd154aed3ba010025
Eman <emanmagdy961@gmail.com>
2020-02-09 01:38:08 +0200

    add delete record function

--- deleteFromTable.sh
+++ deleteFromTable.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 function deleteFromTable {
-    echo "Delete From table"
+	printf "${YELLOW}${bold}Enter the Number of Record you want to Delete from $tableName: ${NC}"
+	typeset -i recordNo;
+	read recordNo;
+	if ! [[ $recordNo ~= ^[0-9]+$ ]]
+	then
+	printf "${RED}${bold} $recordNO Not valid,Enter valid Number${NC}\n"
+	elif [[ $recordNo -le 0 ]]
+	then 
+	printf "${RED}${bold} $recordNO Not valid,Enter valid Number${NC}\n"
+	else
+	recordNo=$recordNo+1;
+	sed -i "$recordNo d" $tableName;
+	printf "${BLUE}${bold} $recordNO Deleted Successfully ${NC}\n"
+	fi
+
 }
