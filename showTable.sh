d5bbf7ea0b770080b91172369575b5df04767820
MohamadFeeshar <mohamed.feeshar@gmail.com>
2020-02-10 00:34:48 +0200

    revert to old showtable

--- showTable.sh
+++ showTable.sh
@@ -2,12 +2,10 @@
 
 function showTable {
     printf "${YELLOW}${bold}Show Table Content Here${NC}\n"
-    #cat $tableName 2> /dev/null
-	awk '{for( i=0; i<=NF; i+=2) $i= "|" }' $tableName
-	#awk '{if(NF%2 != 0)print}' $tableName
-    #if [[ $? != 0 ]]
-    #then
-    #printf "${RED}${bold}Error: Displaying Table${NC}"
-    #fi
+    cat $tableName 2> /dev/null
+    if [[ $? != 0 ]]
+    then
+    	printf "${RED}${bold}Error: Displaying Table${NC}"
+    fi
 	
 }
